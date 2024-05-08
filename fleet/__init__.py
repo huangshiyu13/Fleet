1575cef0e1a34084cf1947f6ede70b925c63e216
huangshiyu <shiyu.huang@aminer.cn>
2024-05-08 14:35:08 +0800

    update

--- fleet/__init__.py
+++ fleet/__init__.py
@@ -1,5 +1,5 @@
 __TITLE__ = "fleet"
-__VERSION__ = "v0.0.3"
+__VERSION__ = "v0.0.4"
 __DESCRIPTION__ = "Fleet is a generic distributed task distribution framework based on a distributed file system."
 __AUTHOR__ = "Shiyu Huang"
 __version__ = __VERSION__
@@ -12,6 +12,6 @@ assert python_version_list >= [
     8,
     0,
 ], (
-    "OpenRL requires Python 3.8 or newer, but your Python is"
+    "Fleet requires Python 3.8 or newer, but your Python is"
     f" {platform.python_version()}"
 )
